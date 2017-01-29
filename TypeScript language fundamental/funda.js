308b5066abc2a0419c6b275a0568492ff7a7f53d
Jayadip.Jadhav <jjayadip74@gmail.com>
2017-01-29 15:32:33 +0530

    Create variable and pass that varialbe to function using backtic ``

---
+++ TypeScript language fundamental/funda.js
@@ -0,0 +1,10 @@
+var language = "angulerJS2";
+console.log("I am learning " + language + " language");
+function NameOfLangualge() {
+    return language;
+}
+function thisIsVersion(version) {
+    console.log("Version: " + version);
+}
+NameOfLangualge();
+thisIsVersion(4);
---
+++ TypeScript language fundamental/funda.ts
@@ -0,0 +1,14 @@
+// backtic ``
+
+let language : string = `angulerJS2`;
+console.log(`I am learning ${language} language`);
+
+function NameOfLangualge() : string {
+    return language;
+}
+
+function thisIsVersion(version : number) {
+    console.log(`Version: ${version}`)
+}
+NameOfLangualge();
+thisIsVersion(4);
